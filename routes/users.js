74faa7267dabcec3fc3109907aacad8c89423784
askhamza <hamza0askour@gmail.com>
2021-05-03 01:15:07 +0200

    fix bug get users

--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,16 @@ let User = require('../model/user');
 
 
 // AVEC PAGINATION
-function getUsers(req, res) {
-    var aggregateQuery = User.aggregate();
 
-    User.aggregatePaginate(
-      aggregateQuery,
-      {
-        page: parseInt(req.query.page) || 1,
-        limit: parseInt(req.query.limit) || 10,
-      },
-      (err, users) => {
-        if (err) {
-          res.send(err);
+  function getUsers(req, res){
+    User.find((err, users) => {
+        if(err){
+            res.send(err)
         }
-  
+
         res.send(users);
-      }
-    );
-  }
+    }); 
+}
 // Récupérer un utilisateur par son id (GET)
 function getUser(req, res){
     let userId = req.params.id;
